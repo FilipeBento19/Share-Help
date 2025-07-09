08ab4ab4bb4760c7538b8874df2256271bc25252
FilipeBento19 <filipepvp1903@gmail.com>
2025-07-09 14:39:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="topo">
+      <img src="./components/icons/ref.png" alt="">
+      <h1>oi</h1>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  .topo img {
+     padding: 0 0 10vw 0;
+  }
+</style>
